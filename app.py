b360d7fe8c8521e6320c752416f03d2285dd1782
JanhaviDalvi <janhavidalvi3008@gmail.com>
2023-07-08 18:39:34 +0530

    add medicine

--- app.py
+++ app.py
@@ -1,52 +1,201 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, redirect, flash, url_for, request, session
 from pymongo import MongoClient
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, login_user, logout_user, current_user, login_required, UserMixin
+from bson import ObjectId
+from datetime import datetime
+from dotenv import load_dotenv
+import os
+import re
+from forms import RegistrationForm, LoginForm
 
+# Load environment variables from .env file and access them
+load_dotenv() 
+mongodb_uri = os.getenv('MONGODB_URI')
+secret_key = os.getenv('SECRET_KEY')
+
+# app configurations
 app = Flask(__name__)
+app.config['SECRET_KEY'] = secret_key
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
-# database stuff
-client = MongoClient('mongodb+srv://janhavidalvi3008:MJVDpMPK0lqmss08@cluster0.39gujyb.mongodb.net/?retryWrites=true&w=majority')
-print(client)
+# MongoDB stuff
+client = MongoClient(mongodb_uri)
 db = client['pharmacy_db']
 
+def insert_customer(customer_name, customer_phone):
+	# Check if customer with the given phone number already exists
+	existing_customer = db.customer.find_one({"customer_phone": customer_phone})
+	if not existing_customer:
+		customer = {
+			"customer_name": customer_name,
+			"customer_phone": customer_phone
+		}
+		db.customer.insert_one(customer)
+	customer_id = db.customer.find_one({"customer_phone": customer_phone})['_id']
+	return customer_id
+
+# User class that satisfies the requirements of Flask-Login. The User class should inherit from UserMixin, which provides default implementations for the required methods.
+class User(UserMixin):
+	def __init__(self, user_id, username, email):
+		self.id = user_id
+		self.username = username
+		self.email = email
+
+	# This method is required by Flask-Login's UserMixin class. It returns the string representation of the user's ID, which is used for user authentication and session management.
+	def get_id(self):
+		return str(self.id)
+
+#  user loader function that Flask-Login will use to load the user from the database based on the user ID
+@login_manager.user_loader
+def load_user(user_id):
+	# Query the database to find the user by ID
+	user = db.user.find_one({'_id': ObjectId(user_id)})
+	# Return the user object
+	return User(user['_id'], user['username'], user['email'])
+
 @app.route('/')
+@login_required
 def home():
-    return render_template('dashboard.html')
+	cursor = db.medicines.find({})
+	medicines = list(cursor)
+	print(len(medicines))
+	for i in medicines:
+		print(i)
+	return render_template('dashboard.html', medicines=medicines)
+
+@app.route('/search_medicine', methods=["POST"])
+@login_required
+def search_medicine():
+	search_term = request.form.get('searchMedicine')
+	regex_pattern = re.compile(f".*{search_term}.*", re.IGNORECASE)
+	query = {'mdcn_name': regex_pattern}
+	results = db.medicines.find(query)
+	results = list(results)
+	return render_template('dashboard.html', medicines=results)
+
 
 @app.route('/sales')
+@login_required
 def sales():
-    # Fetch the medicine names from the 'medicines' collection
-    cursor = db.medicines.find({})
-    medicines = list(cursor)
-    print(len(medicines))
-    for i in medicines:
-        print(i)
-    return render_template('sales.html', medicines=medicines)
+	# Fetch the medicine names from the 'medicines' collection
+	cursor = db.medicines.find({})
+	medicines = list(cursor)
+	print(len(medicines))
+	for i in medicines:
+		print(i)
+	return render_template('sales.html', medicines=medicines)
 
 @app.route('/billing', methods=["POST"])
+@login_required
 def billing():
-    data = request.form.get('medicine_name')
-    print(data)
-    return "hii"
+	medicine_names = request.form.getlist('medicine_name[]')
+	medicine_quantities = request.form.getlist('medicine_quantity[]')
+	medicine_prices = request.form.getlist('medicine_price[]')
+	medicine_total_costs = request.form.getlist('medicine_totalCost[]')
+	medicines = []
+	customer_name = request.form.get("customer_name")
+	customer_phone = request.form.get("customer_phone")
+	total_amount = request.form.get("total_amount")
+	payment_method = request.form.get("payment_method")
+	billing_date = datetime.now()
+	for name, quantity, price, total_cost in zip(medicine_names, medicine_quantities, medicine_prices, medicine_total_costs):
+		medicine = {
+			'medicine_name': name,
+			'medicine_quantity': quantity,
+			'medicine_price': price,
+			'medicine_total_cost': total_cost
+		}
+		medicines.append(medicine)
+	customer_id = insert_customer(customer_name, customer_phone)
+	sale = {
+		'medicines': medicines,
+		'total_amount': total_amount,
+		'billing_date': billing_date,
+		'payment_method': payment_method,
+		'customer_id': customer_id
+	}
+	db.sales.insert_one(sale)
+	return render_template('billing.html', medicines=medicines, customer_name=customer_name, total_amount=total_amount, payment_method=payment_method, billing_date=billing_date)
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/add_medicine', methods=["POST", "GET"])
+@login_required
+def add_medicine():
+	current_datetime = datetime.now()
+	if request.method == 'POST':
+		medicine_name = request.form.get("medicine_name")
+		medicine_type = request.form.get("medicine_type")
+		medicine_description = request.form.get("medicine_description")
+		stock_count = request.form.get("stock_count")
+		medicine_mfg = request.form.get("medicine_mfg")
+		medicine_exp = request.form.get("medicine_exp")
+		medicine_company = request.form.get("medicine_company")
+		medicine_price = request.form.get("medicine_price")
+		existing_medicine = db.medicines.find_one({"mdcn_name": medicine_name})
+		if not existing_medicine:
+			medicine = {
+			"mdcn_name": medicine_name,
+			"mdcn_type": medicine_type,
+			"mdcn_description": medicine_description,
+			"mdcn_mfg": medicine_mfg,
+			"mdcn_exp": medicine_exp,
+			"mdcn_company": medicine_company,
+			"stock_count": int(stock_count),
+			"mdcn_price": round(float(medicine_price), 2)
+			}
+			db.medicines.insert_one(medicine)
+			flash("Medicine has been added!", "success")
+			return redirect(url_for('add_medicine'))
+		
+	return render_template("add_medicine.html", current_datetime=current_datetime)
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+	if current_user.is_authenticated:
+		return redirect(url_for('home'))
+	form = RegistrationForm(db) # Passing the db object to the form
+	if form.validate_on_submit():
+		# encrypting the password
+		hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+		print(hashed_password)
+		# to insert the user
+		new_user = {
+			'email': form.email.data,
+			'username': form.username.data,
+			'password': hashed_password
+		}
+		db.user.insert_one(new_user) #inserting in 'user' collection
+
+		flash(f"Account has been created for '{form.username.data}'!", "success")
+		return redirect(url_for('home'))
+	return render_template('register.html', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        # Get the entered username and password from the form
-        username = request.form['username']
-        password = request.form['password']
-        
-        # Check if the username and password are valid (you can replace this with your authentication logic)
-        if username == 'admin' and password == 'password':
-            # Redirect to the home page or dashboard upon successful login
-            return redirect(url_for('home'))
-        else:
-            # If the credentials are invalid, show an error message on the login page
-            error_message = 'Invalid username or password'
-            return render_template('login.html', error_message=error_message)
-    
-    # Render the login page template
-    return render_template('login.html')
+	if current_user.is_authenticated:
+		return redirect(url_for('home'))
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = db.user.find_one({'username': form.username.data})
+		if user and bcrypt.check_password_hash(user['password'], form.password.data):
+			# Create a User object from the retrieved data
+			user_obj = User(user['_id'], user['username'], user['email'])
+			login_user(user_obj, remember=form.remember.data)  # Login the user
+			flash('You have been logged in!', 'success')
+			return redirect(url_for('home'))
+		else:
+			flash('Login Unsuccessful. Please check username and password', 'danger')
+	return render_template('login.html', form=form)
 
+@app.route("/logout")
+@login_required
+def logout():
+	logout_user()  
+	flash('You have been logged out!', 'success')
+	return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,75 @@
+// to set time for flashed messages in 'layout.html'
+var flash_div = document.getElementById("flash_div");
+if (flash_div) {
+	setTimeout(function () {
+		flash_div.style.display = "none";
+	}, 8000); // Hide the div after 8 seconds
+}
+
+function calculateTotalCost() {
+    var total = 0;
+    var totalCostElements = document.querySelectorAll('#medicine-table-body td:nth-child(4)');
+    var total_hidden = document.getElementById("total_amount");
+
+    totalCostElements.forEach(function (element) {
+        var cost = parseFloat(element.textContent);
+        if (!isNaN(cost)) {
+            total += cost;
+        }
+    });
+
+    var totalCostElement = document.getElementById('total-cost');
+    totalCostElement.textContent = total.toFixed(2);
+    total_hidden.setAttribute("value", totalCostElement.textContent);
+}
+
+function addMedicine() {
+    console.log("add pressed!")
+    // Get the selected medicine name and quantity
+    var name_price = document.getElementById('productName').value;
+    name_price = name_price.split(',');
+    var medicine_name = name_price[0];
+    var medicine_price = parseFloat(name_price[1]);
+    var quantity = parseFloat(document.getElementById('productQuantity').value);
+
+    // Calculate the total cost
+    var totalCost = quantity * medicine_price;
+    // Create a new table row
+    var row = document.createElement('tr');
+    // Add the medicine name, quantity, price, and total cost to the table row
+    row.innerHTML = `
+        <td name='medicine_name'>${medicine_name}</td>
+        <td name='medicine_quantity'>${quantity}</td>
+        <td name='medicine_price'>${medicine_price}</td>
+        <td name='medicine_totalCost'>${totalCost}</td>
+        <td>
+            <input type="hidden" name="medicine_name[]" value="${medicine_name}">
+            <input type="hidden" name="medicine_quantity[]" value="${quantity}">
+            <input type="hidden" name="medicine_price[]" value="${medicine_price}">
+            <input type="hidden" name="medicine_totalCost[]" value="${totalCost}">
+            <button class="btn btn-danger btn-sm delete-btn">Delete</button>
+        </td>
+    `;
+    // Append the new row to the table body
+    document.getElementById('medicine-table-body').appendChild(row);
+
+    // Attach a click event listener to the delete button
+    var deleteButton = row.querySelector('.delete-btn');
+    deleteButton.addEventListener('click', function() {
+        // Find the closest <tr> element (row) and remove it
+        var row = this.closest('tr');
+        row.remove();
+        calculateTotalCost();
+    });
+    calculateTotalCost();
+}
+
+// for 'billing.html'
+function printReceipt() {
+    var printContents = document.getElementById("billing-receipt").innerHTML;
+    var originalContents = document.body.innerHTML;
+
+    document.body.innerHTML = printContents;
+    window.print();
+    document.body.innerHTML = originalContents;
+}
